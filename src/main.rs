2c27920e9f1f6ef16d1f3f5c392a9bb1467fc21f
DeluxeOwl <nospamplease0@protonmail.com>
2021-02-18 12:23:52 +0200

    Refactored to handle errors

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,47 @@
-use std::env;
-use std::fs;
+use std::{env, process};
+use std::{error::Error, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for {} ...", query);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for {} ...", config.query);
 
-    println!("In file {}", filename);
+    println!("In file {}", config.filename);
 
-    let contents =
-        fs::read_to_string(filename).expect("Something went wrong when reading the file.");
+    // doesn't return a value that we want to unwrap
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+// Box<dyn Error> a type that implements the error trait
+// ? will return the error value from the current function for the caller to handle.
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
+
+    // idiomatic way to say we call run for its side effects only
+    // not the return value
+    Ok(())
+}
+struct Config {
+    query: String,
+    filename: String,
+}
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
