84f31ed938ecad7e909b23cb178245e5a7e8aaa1
DeluxeOwl <nospamplease0@protonmail.com>
2021-02-18 12:40:06 +0200

    Refactored more, added tests and the search function

--- src/lib.rs
+++ src/lib.rs
@@ -21,9 +21,40 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     // idiomatic way to say we call run for its side effects only
     // not the return value
     Ok(())
+}
+// In this case, we indicate that the returned vector should contain
+// string slices that reference slices of the argument contents (rather than the argument query).
+// In other words, we tell Rust that the data returned by the search function
+// will live as long as the data passed into the search function in the contents argument.
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,6 @@ fn main() {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    println!("Searching for {} ...", config.query);
-
-    println!("In file {}", config.filename);
 
     // doesn't return a value that we want to unwrap
     if let Err(e) = minigrep::run(config) {
